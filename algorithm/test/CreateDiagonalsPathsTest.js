4c6641bf9b4beddfbf4dfdbc817247358cca466e
unknown <kubex321@gmail.com>
2015-05-30 18:55:44 +0200

    createDiagonalsPaths implementation

--- algorithm/test/CreateDiagonalsPathsTest.js
+++ algorithm/test/CreateDiagonalsPathsTest.js
@@ -15,8 +15,8 @@ describe("createDiagonalsPaths", function() {
             paths = fasta.createDiagonalsPaths(diagonals);
 
         expect(paths.length).toEqual(2);
-        expect(paths[0].diagonals).toEqual(diagonals[0]);
-        expect(paths[1].diagonals).toEqual(diagonals[1]);
+        expect(paths[0].diagonals[0]).toEqual(diagonals[0]);
+        expect(paths[1].diagonals[0]).toEqual(diagonals[1]);
     });
 
     it("should return 4 paths for given diagonals, with 2 start points", function() {
@@ -32,7 +32,7 @@ describe("createDiagonalsPaths", function() {
         expect(paths.length).toEqual(4);
     });
 
-    it("should return 6 paths for given diagonals", function() {
+    it("should return 4 paths for given diagonals", function() {
         var diagonals = [
                 new fasta.Diagonal([0, 0], [2, 2], 1),
                 new fasta.Diagonal([4, 2], [6, 4], 2),
@@ -43,10 +43,10 @@ describe("createDiagonalsPaths", function() {
             ],
             paths = fasta.createDiagonalsPaths(diagonals);
 
-        expect(paths.length).toEqual(6);
+        expect(paths.length).toEqual(4);
     });
 
-    it("should return 6 paths for given diagonals, not sorted before", function() {
+    it("should return 4 paths for given diagonals, not sorted before", function() {
         var diagonals = [
                 new fasta.Diagonal([2, 4], [6, 8], 3),
                 new fasta.Diagonal([6, 5], [10, 9], 5),
@@ -57,7 +57,7 @@ describe("createDiagonalsPaths", function() {
             ],
             paths = fasta.createDiagonalsPaths(diagonals);
 
-        expect(paths.length).toEqual(6);
+        expect(paths.length).toEqual(4);
         for (var i = 0; i < paths.length; i++) {
             expect(paths[i].diagonals[0].score).toEqual(1);
         }
@@ -65,7 +65,7 @@ describe("createDiagonalsPaths", function() {
 
     it("should return 1 path for 2 diagonals with same x, different y", function() {
         var diagonals = [new fasta.Diagonal([0, 0], [2, 2]), new fasta.Diagonal([2, 3], [4, 5])],
-            paths = fasta.createDiagonalsPaths(diagonals, 0);
+            paths = fasta.createDiagonalsPaths(diagonals);
 
         expect(paths.length).toEqual(1);
         expect(paths[0].diagonals).toEqual(diagonals);
@@ -73,7 +73,7 @@ describe("createDiagonalsPaths", function() {
 
     it("should return 1 path for 2 diagonals with same x, different y and not sorted before", function() {
         var diagonals = [new fasta.Diagonal([2, 3], [4, 5]), new fasta.Diagonal([0, 0], [2, 2])],
-            paths = fasta.createDiagonalsPaths(diagonals, 0);
+            paths = fasta.createDiagonalsPaths(diagonals);
 
         expect(paths.length).toEqual(1);
         expect(paths[0].diagonals[0]).toEqual(diagonals[1]);
@@ -82,29 +82,97 @@ describe("createDiagonalsPaths", function() {
 
     it("should return 1 path for 2 diagonals with different x and same y", function() {
         var diagonals = [new fasta.Diagonal([0, 0], [2, 2]), new fasta.Diagonal([3, 2], [5, 4])],
-            paths = fasta.createDiagonalsPaths(diagonals, 0);
+            paths = fasta.createDiagonalsPaths(diagonals);
 
         expect(paths.length).toEqual(1);
         expect(paths[0].diagonals).toEqual(diagonals);
     });
 
     it("should return 1 path for 2 diagonals with different x and y", function() {
+        var diagonals = [new fasta.Diagonal([0, 0], [2, 2]), new fasta.Diagonal([4, 3], [6, 5])],
+            paths = fasta.createDiagonalsPaths(diagonals);
 
+        expect(paths.length).toEqual(1);
+        expect(paths[0].diagonals).toEqual(diagonals);
     });
 
     it("should return 2 paths, both with 2 diagonals, from 3 diagonals, where 2 of them are overlapping, both with same y value", function() {
+        var diagonals = [new fasta.Diagonal([0, 0], [2, 2]), new fasta.Diagonal([3, 2], [5, 4]), new fasta.Diagonal([4, 2], [5,4])],
+            paths = fasta.createDiagonalsPaths(diagonals);
 
+        expect(paths.length).toEqual(2);
+        expect(paths[0].diagonals[0]).toEqual(diagonals[0]);
+        expect(paths[0].diagonals[1]).toEqual(diagonals[1]);
+
+        expect(paths[1].diagonals[0]).toEqual(diagonals[0]);
+        expect(paths[1].diagonals[1]).toEqual(diagonals[2]);
     });
 
     it("should return 2 paths, both with 2 diagonals, from 3 diagonals, where 2 of them are overlapping, both with different y value", function() {
+        var diagonals = [new fasta.Diagonal([0, 0], [2, 2]), new fasta.Diagonal([3, 2], [5, 4]), new fasta.Diagonal([2, 3], [4,5])],
+            paths = fasta.createDiagonalsPaths(diagonals);
 
+        expect(paths.length).toEqual(2);
+        expect(paths[0].diagonals[0]).toEqual(diagonals[0]);
+        expect(paths[0].diagonals[1]).toEqual(diagonals[2]);
+
+        expect(paths[1].diagonals[0]).toEqual(diagonals[0]);
+        expect(paths[1].diagonals[1]).toEqual(diagonals[1]);
     });
 
-    it("should return 2 paths from 3 not overlapping diagonals (one with 3 diagonals and one with 2)", function() {
+    it("should return 1 paths from 3 not overlapping diagonals", function() {
+        var diagonals = [new fasta.Diagonal([0, 0], [2, 2]), new fasta.Diagonal([3, 2], [5, 4]), new fasta.Diagonal([5, 6], [7, 8])],
+            paths = fasta.createDiagonalsPaths(diagonals);
 
+        expect(paths.length).toEqual(1);
+        expect(paths[0].diagonals[0]).toEqual(diagonals[0]);
+        expect(paths[0].diagonals[1]).toEqual(diagonals[1]);
+        expect(paths[0].diagonals[2]).toEqual(diagonals[2]);
     });
 
-    it("should return 4 paths from 5 diagonals", function() {
+    it("should return 1 paths from 4 not overlapping diagonals", function() {
+        var diagonals = [
+                new fasta.Diagonal([0, 0], [2, 2]),
+                new fasta.Diagonal([3, 2], [5, 4]),
+                new fasta.Diagonal([5, 6], [7, 8]),
+                new fasta.Diagonal([9, 8], [11, 10])
+            ],
+            paths = fasta.createDiagonalsPaths(diagonals);
 
+        expect(paths.length).toEqual(1);
+        expect(paths[0].diagonals[0]).toEqual(diagonals[0]);
+        expect(paths[0].diagonals[1]).toEqual(diagonals[1]);
+        expect(paths[0].diagonals[2]).toEqual(diagonals[2]);
+        expect(paths[0].diagonals[3]).toEqual(diagonals[3]);
+    });
+
+    it("should return 2 paths from 5 diagonals, from which 2 are overlapping", function() {
+        var diagonals = [
+                new fasta.Diagonal([0, 0], [2, 2]),
+                new fasta.Diagonal([3, 2], [5, 4]),
+                new fasta.Diagonal([2, 4], [4, 6]),
+                new fasta.Diagonal([5, 6], [7, 8]),
+                new fasta.Diagonal([9, 8], [11, 10])
+            ],
+            paths = fasta.createDiagonalsPaths(diagonals);
+
+        expect(paths.length).toEqual(2);
+        expect(paths[0].diagonals[0]).toEqual(diagonals[0]);
+        expect(paths[1].diagonals[0]).toEqual(diagonals[0]);
+        //TODO: asserts
+    });
+
+    it("should return 3 paths from 5 diagonals", function() {
+        var diagonals = [
+                new fasta.Diagonal([0, 0], [2, 2]),
+                new fasta.Diagonal([4, 2], [8, 6]),
+                new fasta.Diagonal([4, 3], [7, 5]),
+                new fasta.Diagonal([2, 4], [7, 9]),
+                new fasta.Diagonal([7, 8], [9, 10])
+            ],
+            paths = fasta.createDiagonalsPaths(diagonals);
+
+        expect(paths.length).toEqual(3);
+        //TODO: asserts
     });
 });
--- build/fasta.js
+++ build/fasta.js
@@ -283,82 +283,94 @@ var fasta;
 var fasta;
 (function (fasta) {
 
-    function createDiagonalsPaths(diagonals, maxGap) {
+    function createDiagonalsPaths(diagonals) {
+        var sortedDiagonals = diagonals.slice().sort(compareDiagonalsByStart),
+            startingDiagonals = getStartingDiagonals(sortedDiagonals),
+            partialPaths,
+            resultPaths = [];
 
-        return createPaths(diagonals, 0);
+        for (var i = 0; i < startingDiagonals.length; i++) {
+            partialPaths = create([startingDiagonals[i]], sortedDiagonals.slice(i + 1), [], []);
+            resultPaths = resultPaths.concat(partialPaths);
+        }
+        return resultPaths;
+    }
 
-        function createPaths(diagonals, maxGapLength) {
+    function compareDiagonalsByStart(first, second) {
+        return first.startPoint[0] - second.startPoint[0] || first.startPoint[1] - second.startPoint[1];
+    }
 
-            return create([diagonals[0]], diagonals.slice(1), [], []);
-
-            function create(current, rest, result, previous) {
-                var nextDiagonal;
-                if (current.length === 0 && rest.length === 0)
-                    return;
-                if (rest.length === 0) {
-                    console.log('push');
-                    for (var i = 0; i < current.length; i++) {
-                        console.log(current[i].score);
-                    }
-                    result.push(new fasta.DiagonalsPath(current));
-                } else {
-                    nextDiagonal = rest[0];
-                    console.log('new');
-                    for (var j = 0; j < current.length; j++) {
-                        console.log(current[j].score);
-                    }
-                    console.log('next');
-                    console.log(nextDiagonal.score);
-                    if (nextStartsAfter(current, nextDiagonal)) {
-                        if (nextAfterPrevious(nextDiagonal, previous)) {
-                            return result;
-                        }
-                        if (rest.slice(1).length === 0) {
-                            console.log('push 2');
-                            var arr = current.slice();
-                            arr.push(nextDiagonal);
-                            result.push(new fasta.DiagonalsPath(arr));
-                        } else {
-                            var newArray = current.slice();
-                            newArray.push(nextDiagonal);
-
-                            console.log('a');
-                            create(newArray, rest.slice(1), result, []);
-                            console.log('b');
-
-                            var newPrevious = previous.slice();
-                            newPrevious.push(nextDiagonal);
-                            create(current, rest.slice(1), result, newPrevious);
-                        }
-                    } else {
-                        console.log('c');
-                        create(current, rest.slice(1), result, previous);
-                    }
-                }
-                return result;
+    function getStartingDiagonals(diagonals) {
+        var firstDiagonal = diagonals[0],
+            startingDiagonals = [firstDiagonal];
+        for (var i = 1; i < diagonals.length; i++) {
+            if (nextCanBeJoinedWithCurrent([firstDiagonal], diagonals[i])) {
+                return startingDiagonals;
             }
+            startingDiagonals = startingDiagonals.concat(diagonals[i]);
+        }
 
-            function nextStartsAfter(current, next) {
-                var currentEnd = current[current.length - 1].endPoint,
-                    nextStart = next.startPoint;
+        return startingDiagonals;
+    }
 
-                return (nextStart[0] >= currentEnd[0] && nextStart[1] >= currentEnd[1]);
+    function lastDiagonal(rest) {
+        return rest.slice(1).length === 0;
+    }
+
+    function create(current, rest, result, previous) {
+        var nextDiagonal;
+        if (current.length === 0 && rest.length === 0)
+            return;
+
+        if (rest.length === 0) {
+            result.push(new fasta.DiagonalsPath(current));
+            return result;
+        }
+
+        nextDiagonal = rest[0];
+
+        if (nextCanBeJoinedWithAnyFromPrevious(nextDiagonal, previous)) {
+            //as diagonals are sorted, if next can be joined with any from previously joined,
+            //there is no need to check the rest of them, as they all can be joined the same way
+            return result;
+        }
+
+        if (nextCanBeJoinedWithCurrent(current, nextDiagonal)) {
+            if (lastDiagonal(rest)) {
+                result.push(new fasta.DiagonalsPath(current.concat([nextDiagonal])));
+            } else {
+                //go deeper with next diagonal joined current
+                create(current.concat([nextDiagonal]), rest.slice(1), result, []);
+
+                //continue without next diagonal
+                create(current, rest.slice(1), result, previous.concat([nextDiagonal]));
             }
+        } else {
+            //skip next diagonal and continue
+            create(current, rest.slice(1), result, previous);
+        }
+        return result;
+    }
 
-            function nextAfterPrevious(next, previous) {
-                var nextStart = next.startPoint,
-                    previousEnd;
+    function nextCanBeJoinedWithCurrent(current, next) {
+        var currentEnd = current[current.length - 1].endPoint,
+            nextStart = next.startPoint;
 
-                for (var i = 0; i < previous.length; i++) {
-                    previousEnd = previous[i].endPoint;
+        return (nextStart[0] >= currentEnd[0] && nextStart[1] >= currentEnd[1]);
+    }
 
-                    if (nextStart[0] <= previousEnd[0] && nextStart[1] <= previousEnd[1]) {
-                        return false
-                    }
-                }
-                return previous.length !== 0;
+    function nextCanBeJoinedWithAnyFromPrevious(next, previous) {
+        var nextStart = next.startPoint,
+            previousEnd;
+
+        for (var i = 0; i < previous.length; i++) {
+            previousEnd = previous[i].endPoint;
+
+            if (nextStart[0] >= previousEnd[0] && nextStart[1] >= previousEnd[1]) {
+                return true;
             }
         }
+        return false;
     }
 
     fasta.createDiagonalsPaths = createDiagonalsPaths;
